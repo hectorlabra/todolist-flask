c7bab99fcedc05797fabe5350104c3ef3127ca02
Héctor Labra <121694325+hectorlabra@users.noreply.github.com>
2023-09-17 18:58:28 +0000

    finished excercises

--- src/app.py
+++ src/app.py
@@ -3,37 +3,31 @@ from flask import Flask, jsonify
 from flask import request
 
 
-
-
 app = Flask(__name__)
 
 todos = [
-    { "label": "My first task", "done": False },
-    { "label": "My second task", "done": False }
+    {"label": "My first task", "done": False},
+    {"label": "My second task", "done": False}
 ]
 
 
 @app.route('/todos', methods=['GET'])
-
 def hello_world():
-    json_text=jsonify(todos)
+    json_text = jsonify(todos)
     return json_text
 
 
-todos = [{ "label": "", "done": False }]
+todos = [{"label": "", "done": False}]
+
 
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
     request_body = request.json
     print("Incoming request with the following body", request_body)
-    
+
     todos.append(request_body)
-    
+
     return jsonify(todos)
-
-
-
-
 
 
 @app.route('/todos/<int:position>', methods=['DELETE'])
@@ -43,7 +37,7 @@ def delete_todo(position):
         return jsonify(todos)
     else:
         return 'Invalid position', 400
-        
+
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=3245, debug=True)
+    app.run(host='0.0.0.0', port=3245, debug=True)
